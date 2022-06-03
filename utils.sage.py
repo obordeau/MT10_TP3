32f28b16fc9e0753735f4bb98676ad6fd77934ee
Martin <martin.schneider2001@gmail.com>
2022-06-03 12:07:55 +0200

    rapport

--- utils.sage.py
+++ utils.sage.py
@@ -49,5 +49,61 @@ def Syndrome(q, k, code, V, A):
 		for j in range(1, r):
 			sum += pow(FqX(A[i]) * X,j)
 		polynome = polynomeLagrange(q, A, i)
-		result += FqX(code[i]) * (pow(FqX(V[i]),-1) * pow(polynome(FqX(A[i])),-1)) * sum 
+		result += FqX(code[i]) * (pow(FqX(V[i]),-1) * pow(polynome(FqX(A[i])),-1)) * sum
 	return result
+
+def test1(n):
+    for i in range(1,n + 1):
+        if moebius(i) not in [-1, 0, 1]:
+            return False
+    return True
+
+def test2(n):
+    for i in range(1,n + 1):
+        sum = 0
+        for d in divisors(i):
+            sum += moebius(d)
+        if (i == 1 and sum != 1) or (i > 1 and sum != 0):
+            return False
+    return True
+
+def phiMobius(n):
+    p = 0
+    for d in divisors(n):
+        p += moebius(n /d ) * d
+    return p
+
+def irr(p, n):
+    res = 0
+    for d in divisors(n):
+        res += moebius(n/d) * pow(p, d)
+    return res/n
+
+def polynomes10():
+    number = 0
+    poly = []
+    if X.is_irreducible() : 
+        poly.append(X)
+        number += 1
+    for n in range (2,2048):
+        P = 0
+        i = 0
+        for c in (bin(n)[2:]):
+            k = int(c)
+            P += (X^i)*k
+            i += 1
+        if not(P in poly) :
+            if P.is_irreducible() :
+                poly.append(P)
+                number += 1
+    print(number)
+
+p = 2
+n = 6
+F2 = GF(p)
+R = F2.polynomial_ring()
+prod = R.one() #utilise pour le produit
+for d in divisors(n):
+  for poly in R.polynomials(of_degree = d):
+    if poly.is_irreducible() and poly.is_monic(): # si irreductible et unitaire
+      prod *= poly
